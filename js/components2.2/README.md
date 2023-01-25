158eeec349539049b127b4dd588c2f8cabd131ec
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-01-25 12:28:18 +0100

    lesson task 2.2

---
+++ js/components2.2/README.md
@@ -0,0 +1,22 @@
+# JavaScript 2 - Module 2
+
+## Lesson Task 2 Question
+
+**Level 1**
+
+Based on the below array of users, create a reusable function that will remove any user matching the ID that function gets passed.
+
+The function should accept one argument for the id, filter out any matches and return the updated array.
+
+```js
+let users = [
+  { id: 1, name: "Emma", gender: "Female" },
+  { id: 2, name: "Oscar", gender: "Male" },
+  { id: 3, name: "Alexis", gender: "Female" },
+  { id: 4, name: "Adam", gender: "Male" },
+];
+```
+
+**Level 2** _(optional)_
+
+Convert the filter function you created in Level 1 into a one-line function.
---
+++ js/components2.2/script.js
@@ -0,0 +1,37 @@
+// level 1
+
+let users = [
+    { id: 1, name: "Emma", gender: "Female" },
+    { id: 2, name: "Oscar", gender: "Male" },
+    { id: 3, name: "Alexis", gender: "Female" },
+    { id: 4, name: "Adam", gender: "Male" },
+  ];
+
+/* my version:
+
+function filterUsers(idToRemove) {
+    const filteredUsers = users.filter(filterFunction);
+
+    function filterFunction(user) {
+        if(user.id !== idToRemove) {
+            return true;
+        }
+    }
+
+    users = filteredUsers;
+}
+
+filterUsers(1);
+console.log(users);
+
+*/
+
+// level 2
+
+const filterUsers = (idToRemove) => users.filter((user) => user.id !== idToRemove);
+
+users = filterUsers(2);
+// Jan wrote:
+// const filteredUsers = filterUsers(2);
+
+console.log(users);
